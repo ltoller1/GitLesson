a58b47fae4c21572b9d524221582b676c131b20c
John Ramirez <jramirez@blindbrook.org>
2020-12-02 20:58:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# WASSSUUUUUUPPPPPPPPPP
+#suh dude
+
+darren = 5
+
+if darren == 6:
+  print("WELCOME TO THE BEST CLUB IN BLIND BROOK!")
+  print("BLA BLA BLA")
+else:
+  print(darren)
+
+
+def sort_list():
+    pass
+
+# file --> * make changes to the file* --> commit the file 
+# next day --> make more file changes --> complete the function --> commit the file
+# take commited file --> push it to github
+
